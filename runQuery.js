15d5a86fed90dc6a03bd6ff3fe0bbfbd9d563b54
hdemirev <hari.demirev@gmail.com>
2021-10-15 14:15:10 -0400

    make response data json

--- runQuery.js
+++ runQuery.js
@@ -14,7 +14,7 @@ const run = async() => {
     const jobId = process.env.RENDER_INSTANCE_ID.substring(0,24)
     const userQuery = unescape(process.argv.slice(2)[0])
     const userResponse = await pool.query(userQuery)
-    await pool.query('insert into query_results(job_id, response) VALUES($1, $2)', [jobId, userResponse.rows])
+    await pool.query('insert into query_results(job_id, response) VALUES($1, $2)', [jobId, {responseData: userResponse.rows}])
     process.exit(0)
 }
 
