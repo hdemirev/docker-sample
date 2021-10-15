8f6a334b7400a6f462038930fed7a44fb5b8f80b
hdemirev <hari.demirev@gmail.com>
2021-10-15 14:05:29 -0400

    add quotes around query

--- server.js
+++ server.js
@@ -25,19 +25,23 @@ app.get("/", (req, res) => {
 });
 
 const createJob = async(query) => {
-  const response = await fetch(`https://api.staging.render.com/v1/services/${process.env.RENDER_SERVICE_ID}/jobs`, {
-    method: 'post',
-    body: JSON.stringify({
-      startCommand: `QUERY=${query} node runQuery.js`,
-      planId: 'plan-srv-007'
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
-    }
-  });
-  const data = await response.json();
-  return data
+  try{
+    const response = await fetch(`https://api.staging.render.com/v1/services/${process.env.RENDER_SERVICE_ID}/jobs`, {
+      method: 'post',
+      body: JSON.stringify({
+        startCommand: `QUERY="${query}"" node runQuery.js`,
+        planId: 'plan-srv-007'
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
+      }
+    });
+    const data = await response.json();
+    return data
+  } catch(e){
+    console.log(e)
+  }
 }
 
 const checkJob = async(id) => {
