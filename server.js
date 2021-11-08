af24c5de75dab075056df0007db17232763d3398
hdemirev <hari.demirev@gmail.com>
2021-11-08 11:13:05 -0800

    add read-file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const fs = require('fs')
 
 // Constants
 const PORT = 8080;
@@ -9,6 +10,13 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
+  console.log("handling request, k revision: ", process.env.K_REVISION);
+  res.send("Hello World, k revision: " + process.env.K_REVISION);
+});
+
+app.get("/read-file", (req, res) => {
+  const data = fs.readFileSync('/etc/secrets/.env')
+  res.send(data.toString())
   console.log("handling request, k revision: ", process.env.K_REVISION);
   res.send("Hello World, k revision: " + process.env.K_REVISION);
 });
