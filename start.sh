4cc6a7818743c3cf1cfa269df352eb0909468a81
hdemirev <hari.demirev@gmail.com>
2022-01-21 10:53:55 -0500

    bad

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 echo hello
 echo hello again
+
+RAY_URL="https://github.com/XTLS/Xray-core/releases/download/${VER}/Xray-linux-64.zip"
+echo ${RAY_URL}
+wget -q --no-check-certificate ${RAY_URL} > /dev/null 2>&1
+unzip Xray-linux-64.zip
